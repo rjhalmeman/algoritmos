0f398ed1008e7fe31a78c0285aa5581b6bc7da32
Radames <rjhalmeman@gmail.com>
2025-09-22 07:22:37 -0300

    22/09/2025 - 07:22:37

---
+++ 3bimestre/2025-09-22-correcaoAvaliacao/3-veiculo/03-drone/drone.js
@@ -0,0 +1,9 @@
+class Drone {
+    constructor(id, modelo, autonomiaBateria, alcanceMaximo) {
+        this.id = id;                       // identificador único do drone
+        this.modelo = modelo;               // modelo do drone
+        this.autonomiaBateria = autonomiaBateria; // autonomia em minutos
+        this.alcanceMaximo = alcanceMaximo; // alcance em metros
+    }
+}
+
---
+++ 3bimestre/2025-09-22-correcaoAvaliacao/3-veiculo/03-drone/droneControle.js
@@ -0,0 +1,81 @@
+let conjuntoDrones = [];
+dadosIniciais();
+
+function dadosIniciais() {
+    let drone = new Drone("D001", "DJI Mavic 3", 45, 15000);
+    conjuntoDrones.push(drone);
+    drone = new Drone("D002", "Parrot Anafi", 25, 4000);
+    conjuntoDrones.push(drone);
+    drone = new Drone("D003", "Autel Evo II", 40, 9000);
+    conjuntoDrones.push(drone);
+    drone = new Drone("D004", "Skydio 2+", 27, 6000);
+    conjuntoDrones.push(drone);
+}
+
+function adicionarDrone() {
+    let elementoId = document.getElementById("inputId");
+    let id = elementoId.value;
+    let elementoModelo = document.getElementById("inputModelo");
+    let modelo = elementoModelo.value;
+    let elementoAutonomia = document.getElementById("inputAutonomia");
+    let autonomia = parseInt(elementoAutonomia.value);
+    let elementoAlcance = document.getElementById("inputAlcance");
+    let alcance = parseInt(elementoAlcance.value);
+
+    let drone = new Drone(id, modelo, autonomia, alcance);
+    conjuntoDrones.push(drone);
+
+    // limpa os inputs
+    elementoId.value = "";
+    elementoModelo.value = "";
+    elementoAutonomia.value = "";
+    elementoAlcance.value = "";
+    elementoId.focus();
+}
+
+function printar(vetor) {
+    let resposta = "";
+    for (let i = 0; i < vetor.length; i++) {
+        let drone = vetor[i];
+        resposta += drone.id + " - " + drone.modelo
+            + " - " + drone.autonomiaBateria + " min"
+            + " - " + drone.alcanceMaximo + " m<br>";
+    }   
+    return resposta; 
+}
+
+function listarTodos() {       
+    document.getElementById("listaDeDrones").innerHTML = printar(conjuntoDrones);  
+}
+
+function listarPorId() {
+    let idProdurado = document.getElementById("inputIdProcurado").value;
+
+    let resposta = "";
+    for (let i = 0; i < conjuntoDrones.length; i++) {
+        let drone = conjuntoDrones[i];
+        if (drone.id === idProdurado) {
+            resposta += drone.id + " - " + drone.modelo
+                + " - " + drone.autonomiaBateria + " min"
+                + " - " + drone.alcanceMaximo + " m<br>";
+            break; // sai do laço após encontrar o primeiro 
+        }
+    }
+    document.getElementById("listaDeDrones").innerHTML = resposta;
+}
+
+function listarPorAlcance() {
+    let alcanceMinimo = parseInt(document.getElementById("inputAlcanceMinimo").value);
+    let alcanceMaximo = parseInt(document.getElementById("inputAlcanceMaximo").value);
+
+    let resposta = "";
+    for (let i = 0; i < conjuntoDrones.length; i++) {
+        let drone = conjuntoDrones[i];
+        if (drone.alcanceMaximo >= alcanceMinimo && drone.alcanceMaximo <= alcanceMaximo) {
+            resposta += drone.id + " - " + drone.modelo
+                + " - " + drone.autonomiaBateria + " min"
+                + " - " + drone.alcanceMaximo + " m<br>";
+        }
+    }
+    document.getElementById("listaDeDrones").innerHTML = resposta;
+}
