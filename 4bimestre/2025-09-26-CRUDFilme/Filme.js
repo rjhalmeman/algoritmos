4134047ba7c36b91677db9aeacfdd21f80335695
Radames <rjhalmeman@gmail.com>
2025-09-26 12:21:05 -0300

    Aula do dia 26/09/2025

---
+++ 4bimestre/2025-09-26-CRUDFilme/Filme.js
@@ -0,0 +1,11 @@
+class Filme {
+    constructor(id, nome, ano,50, posicaoNaLista) {
+        this.id = id;
+        this.nome = nome;
+        this.ano = ano;
+        this.50 = 50;
+
+
+        this.posicaoNaLista = posicaoNaLista; //atributo para facilitar a alteração e exclusão 
+    }
+}
