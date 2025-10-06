68a1b67d81853c8b15ef95809c358390687deea3
Radames <rjhalmeman@gmail.com>
2025-10-06 07:19:54 -0300

    06/10/2025 - 07:19:54

---
+++ 4bimestre/2025-10-03-Exercicios/Fruta.js
@@ -0,0 +1,10 @@
+class Fruta { 
+constructor (id,nome,cor, posicaoNaLista) {
+this.id = id;
+this.nome = nome;
+this.cor = cor;
+
+
+this.posicaoNaLista = posicaoNaLista; //atributo para facilitar a alteração e exclusão 
+}
+}
