df394dd3533acbf1ff938b79bd4bb05657dc25eb
Radames <rjhalmeman@gmail.com>
2025-10-24 06:49:29 -0300

    24/10/2025 - 06:49:29

---
+++ 4bimestre/2025-10-24-Vídeo/crudFilme/Filme.js
@@ -0,0 +1,12 @@
+class Filme {
+    constructor(id, titulo,diretor,dataLancamento, avaliacao, posicaoNaLista) {
+        this.id = id;
+        this.titulo = titulo;
+        this.diretor = diretor;
+        this.dataLancamento = dataLancamento;
+        this.avaliacao = avaliacao;
+
+
+        this.posicaoNaLista = posicaoNaLista; //atributo para facilitar a alteração e exclusão 
+    }
+}
---
+++ 4bimestre/2025-10-24-Vídeo/crudFilme/videoNoYoutube.md
@@ -0,0 +1,3 @@
+O link abaixo mostra um vídeo do passo a passo de como esse CRUD foi feito.
+
+https://youtu.be/lZaw_leSSEc
